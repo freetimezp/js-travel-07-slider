2086846323a9fd465e695f929238bdfe86a225fe
freetimezp <kraftukrnet@ukr.net>
2023-07-18 00:39:42 +0300

    05. js-travel-07-slider. Slider func.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,35 @@
+const slides = document.querySelectorAll('.slide');
+const nextBtn = document.querySelector('.next-btn');
+const prevBtn = document.querySelector('.prev-btn');
+
+const numberOfSlides = slides.length;
+let slideNumber = 0;
+
+nextBtn.onclick = () => {
+    slides.forEach((slide) => {
+        slide.classList.remove('active');
+    });
+
+    slideNumber++;
+
+    if (slideNumber > (numberOfSlides - 1)) {
+        slideNumber = 0;
+    }
+
+    slides[slideNumber].classList.add('active');
+}
 
 
-//slider next button
+prevBtn.onclick = () => {
+    slides.forEach((slide) => {
+        slide.classList.remove('active');
+    });
 
+    slideNumber--;
 
-//slider prev button
+    if (slideNumber < 0) {
+        slideNumber = numberOfSlides - 1;
+    }
+
+    slides[slideNumber].classList.add('active');
+}
